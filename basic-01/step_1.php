6c27efeef056fc10e4d919742357115a09e313fa
Ahmad Ghazzi <aghazzi.1998@gmail.com>
2023-02-19 18:20:34 +0200

    step 1 2 and 3

---
+++ basic-01/step_1.php
@@ -0,0 +1,3 @@
+Hello world, Help me I'm using PHP
+<?php
+?>
---
+++ basic-01/step_3.php
@@ -0,0 +1,9 @@
+<?php
+$name = 'Ahmad';
+$email = 'aghazzi.1998@gmail.com';
+echo 'My name is "' . $name . '" and my email is "' . $email . '".';
+$height = 10;
+$width = 8;
+$area = $height * $width;
+echo 'The area is ' . $area . '.';
+?>
